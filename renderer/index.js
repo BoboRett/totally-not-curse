731ab21d4e97a07e0f04d6ea3ae857e8303f60fb
BoboRett <BoboRett@users.noreply.github.com>
2020-09-05 12:04:18 +0100

    Add lodash

--- renderer/index.js
+++ renderer/index.js
@@ -1,6 +1,8 @@
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash';
 
 import App from './app';
 import createStore from './store/redux-store';
