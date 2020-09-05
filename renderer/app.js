9d039a78b863c3372cc3cc984d81374da17a6fb2
BoboRett <BoboRett@users.noreply.github.com>
2020-09-05 13:02:27 +0100

    Find Wow directories
    Needs a lookup into registry keys to find the base install
    dir. Then a poke with fs to find the individual versions of
    Wow that are installed. All are stored.

--- renderer/app.js
+++ renderer/app.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+
+import { setPaths } from './store/wow-client';
 import './app.less';
 
-const App = () => {
+const App = ({ setPaths }) => {
+    useEffect(() => {
+        api.findWow().then(setPaths);
+    }, []);
+
     return (
         <>
             <h1>Boop</h1>
@@ -9,4 +18,12 @@ const App = () => {
     );
 };
 
-export default App;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ setPaths }, dispatch);
+}
+
+App.propTypes = {
+    setPaths: PropTypes.func
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- renderer/store/redux-store.js
+++ renderer/store/redux-store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 
+import wowClient from './wow-client';
+
 export default function() {
-    const store = createStore(() => {});
+    const store = createStore(wowClient.reducer, wowClient.init());
     return store;
 }
