d9fba680454b4f94825de193d6cf2fae01594fe5
BoboRett <BoboRett@users.noreply.github.com>
2020-10-31 18:53:03 +0000

    Let cache be saved without renderer prompt
    Rather than always passing an array of addosn to be
    saved, this opens up the store to the main process so
    that saveAddons can be called without any arguments
    and instead caching whatever is currently in the store.

--- main/cache.js
+++ main/cache.js
@@ -1,13 +1,18 @@
-const { app } = require('electron');
+const { app, webContents } = require('electron');
 const fs = require('fs');
 const path = require('path');
 const Pbf = require('pbf');
 const compile = require('pbf/compile');
 const pbfSchema = require('protocol-buffers-schema');
+const _ = require('lodash');
 
 const schema = compile(pbfSchema.parse(fs.readFileSync(path.resolve(__dirname, '../schema.proto'))));
 
-function saveAddons(addons) {
+async function saveAddons(addons) {
+    if(_.isNil(addons)) {
+        const wc = webContents.getFocusedWebContents();
+        addons = await wc.executeJavaScript('store.getState().addons');
+    }
     const addonsCache = path.resolve(app.getPath('userData'), './addons.cache');
     const pbf = new Pbf();
     schema.Addons.write({ addons }, pbf);
